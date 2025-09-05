599897d7969293c0350885244befdede849073f4
Rahul kumar <rahulsah2728@gmail.com>
2025-09-05 22:27:24 +0530

    Fix component name and InfoBox import case

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,28 +1,27 @@
 import SearchBox from "./SearchBox"
-import InfoBox from "./infoBox"
+import InfoBox from "./InfoBox"    // fix case
 import { useState } from "react"
 
-
-export default function weatherApp(){
- const [WeatherInfo, setWeatherInfo] = useState({
-    city:"Delhi",
+export default function WeatherApp() {    // capitalize component
+  const [WeatherInfo, setWeatherInfo] = useState({
+    city: "Delhi",
     feelslike: 34.23,
     humidity: 3,
     temp: 37.42,
     tempMax: 37.42,
     tempMin: 37.42,
-     weather: "overcast clouds"
- });
+    weather: "overcast clouds"
+  });
 
- let updateInfo =(newInfo) =>{
+  let updateInfo = (newInfo) => {
     setWeatherInfo(newInfo);
- }
+  }
 
-    return (
-        <div style={{textAlign:"center"}}>
-            <h2> Weather app</h2>
-          <SearchBox  updateInfo={updateInfo}/>
-          <InfoBox info={WeatherInfo} />
-        </div>
-    )
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Weather App</h2>
+      <SearchBox updateInfo={updateInfo} />
+      <InfoBox info={WeatherInfo} />
+    </div>
+  )
 }
